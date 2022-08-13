a930901297588ee0a2e0a90c584cbbce70edd469
imjunaid <shopnobazjunaid@gmail.com>
2022-08-13 23:17:28 +0600

    Update

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,12 +1,17 @@
-import API_KEY from '../utils/config.json';
-import moment from 'moment';
+import config from '../utils/config.json';
 const BASE_URL = "https://newsapi.org/v2/everything";
 
-const today = moment().format("YYYY-MM-DD");
-const weekAgo = moment().subtract(7, 'days').format("YYYY-MM-DD");
+// const today = moment().format("YYYY-MM-DD");
+// const weekAgo = moment().subtract(7, 'days').format("YYYY-MM-DD");
+function weekAgo() {
+    return Date.now() - 7 * 24 * 60 * 60;
+  }
+function today() {
+    return Date.now();
+  }
 
 export const searchNews  = (keyword) => {
-    return fetch(`${BASE_URL}?q=${keyword}&from=${weekAgo}&to=${today}&apiKey=${API_KEY}`, {
+    return fetch(`${BASE_URL}?q=${keyword}&from${weekAgo}&to=${today}&sortBypopularity&apiKey=${config.API_KEY}`, {
         method: "GET",
         headers: {
             'Accept': 'application/json',
